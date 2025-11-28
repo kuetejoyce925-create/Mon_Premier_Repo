9cfae630ddf76ed8d817daedfdbc9e4927d3080f
root <root@Ubuntu2.0.myguest.virtualbox.org>
2025-11-28 15:11:55 +0100

    mon premier commit

---
+++ code_python/test.py
@@ -0,0 +1,10 @@
+#Pythob - Indentation Significatve 
+if age < 18: 
+    print ("Mineur")
+    acces = False 
+elif age < 65: 
+    print ("Aldute")
+    acces = True 
+else: 
+    print("Senior")
+    acces = True
